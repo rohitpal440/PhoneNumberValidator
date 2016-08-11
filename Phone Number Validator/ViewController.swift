a9c4d7407f76e9717bb1a871edf2321bb07ed013
Rohit Pal <rohitpal440@gmail.com>
2016-08-11 09:49:49 +0530

    Initial Commit

---
+++ Phone Number Validator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Phone Number Validator
+//
+//  Created by Rohit Pal on 11/08/16.
+//  Copyright © 2016 Rohit Pal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Phone Number ValidatorTests/Phone_Number_ValidatorTests.swift
@@ -0,0 +1,36 @@
+//
+//  Phone_Number_ValidatorTests.swift
+//  Phone Number ValidatorTests
+//
+//  Created by Rohit Pal on 11/08/16.
+//  Copyright © 2016 Rohit Pal. All rights reserved.
+//
+
+import XCTest
+@testable import Phone_Number_Validator
+
+class Phone_Number_ValidatorTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
